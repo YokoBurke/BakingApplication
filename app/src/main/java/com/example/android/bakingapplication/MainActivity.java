8caefb2525ea40ebdf44ceaa065b33fb59cb523d
yoko.fujikuro <fujikurock@gmail.com>
2018-10-08 16:59:42 -0400

    Note for GSON

--- app/src/main/java/com/example/android/bakingapplication/MainActivity.java
+++ app/src/main/java/com/example/android/bakingapplication/MainActivity.java
@@ -1,10 +1,21 @@
 package com.example.android.bakingapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.android.bakingapplication.utilities.NetworkUtils;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String>{
 
     private RecyclerView recyclerView;
 
@@ -13,4 +24,61 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+    @NonNull
+    @Override
+    public Loader<String> onCreateLoader(int id, @Nullable Bundle args) {
+
+        return new AsyncTaskLoader<String>(this) {
+            String mRecipeJson;
+
+            @Override
+            public void onStartLoading() {
+                if (mRecipeJson != null) {
+                    deliverResult(mRecipeJson);
+                } else {
+                    forceLoad();
+                }
+            }
+
+            @Nullable
+            @Override
+            public String loadInBackground() {
+                URL recipeUrl = NetworkUtils.checkURL();
+                String myString = "";
+                try {
+                    myString = NetworkUtils.getResponseFromHttpUrl(recipeUrl);
+
+
+                } catch (IOException e) {
+                    Log.e("Main Activity", "Problem making the HTTP request.", e);
+                }
+
+                return myString;
+            }
+
+            @Override
+            public void deliverResult(String myRecipeJson) {
+                mRecipeJson = myRecipeJson;
+                super.deliverResult(myRecipeJson);
+            }
+
+        };
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<String> loader, String data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<String> loader) {
+
+        }
+
+    }
+
+
 }
