3cb00ecd68eb2e475885541aa89d9a6cf8b3b83d
yoko.fujikuro <fujikurock@gmail.com>
2018-10-10 16:57:21 -0400

    Fragments Added

---
+++ app/src/main/java/com/example/android/bakingapplication/Fragments/IngredientFragment.java
@@ -0,0 +1,8 @@
+package com.example.android.bakingapplication.Fragments;
+
+import android.support.v4.app.Fragment;
+
+
+public class IngredientFragment extends Fragment {
+
+}
---
+++ app/src/main/java/com/example/android/bakingapplication/Fragments/StepsFragment.java
@@ -0,0 +1,8 @@
+package com.example.android.bakingapplication.Fragments;
+
+import android.support.v4.app.Fragment;
+
+
+public class StepsFragment extends Fragment {
+
+}
