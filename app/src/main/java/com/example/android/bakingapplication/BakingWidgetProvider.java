94fa9a2045331fbb4bf843a87d7d20b071e3d79e
YBURKE <fujikurock@gmail.com>
2018-11-25 23:14:20 -0500

    Widget intent implementation step 1 completed

--- app/src/main/java/com/example/android/bakingapplication/BakingWidgetProvider.java
+++ app/src/main/java/com/example/android/bakingapplication/BakingWidgetProvider.java
@@ -15,15 +15,14 @@ public class BakingWidgetProvider extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
-        // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.baking_widget_provider);
         //views.setTextViewText(R.id.appwidget_text, widgetText);
 
-        Intent intent = new Intent(context, DetailActivity.class);
+        Intent intent = new Intent(context, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
-        //views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
+        views.setOnClickPendingIntent(R.id.widget_empty_view, pendingIntent);
+
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
